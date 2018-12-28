dd9fbedd69355e51e3c8415299c847072b133964
haihuam <Haihua>
2018-12-28 11:09:58 +0800

    initial version for flow definition

---
+++ hcflow.py
@@ -0,0 +1,24 @@
+
+# flow_def:
+#     1. Extract location data (index) from hyperspectral image.
+#     2. Extract pixel data, build the patch for training.
+
+
+
+def conf_parser(cfg):
+    """
+    Function: Parse the configure file to run through a whole flow.
+    Input: Configure file path
+    Return: A sequence of functions to run.
+    """
+
+
+def start_flow():
+    """
+    Function: Run through the flow.
+    Input: A sequence of functions.
+    """
+
+if __name__ == "__main__":
+    conf_parser()
+    start_flow()
