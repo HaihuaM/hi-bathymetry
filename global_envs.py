2e9c21c99922aaf6fcef5d1f825c970a2e9e125c
haihuam <Haihua>
2018-12-28 13:03:42 +0800

    setup global envs

---
+++ global_envs.py
@@ -0,0 +1,8 @@
+
+import os.path as op
+
+data_path = '../orig_data/GF5_AHSI_E120.98_N31.81_20180715_000983_L10000006312/'
+
+lat_tiff_file = op.join(data_path, "GF5_AHSI_E120.98_N31.81_20180715_000983_L10000006312.lat.tiff")
+lon_tiff_file = op.join(data_path, "GF5_AHSI_E120.98_N31.81_20180715_000983_L10000006312.lon.tiff")
+geo_tiff_file = op.join(data_path, "GF5_AHSI_E120.98_N31.81_20180715_000983_L10000006312.geotiff")
