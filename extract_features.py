ecd30ae32dd989345628e838bcf59f1688adedc3
haihuam <haihuam@outlook.com>
2019-01-06 13:38:39 +0000

    Add func1 to extract pixel data from hyperspectral image

--- extract_features.py
+++ extract_features.py
@@ -11,7 +11,8 @@ from osgeo import gdal
 from tqdm import tqdm
 from global_envs import *
 from utility import time_stamp
-from coordinate_transform import gdal_reader, load_depth_data_parser
+from coordinate_transform import gdal_reader, load_depth_data_parser, obj_dump
+from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 
 global GEO_TIFF_FILE 
@@ -28,26 +29,67 @@ def load_points_index(points_index_file):
         return points_index
 
 
+def extract_pixel_value_func1(points_index_file):
+    """
+    Function: according to the index in the image to extract pixel values from all bands, not use patch only for single point.
+    parameters: NONE
+    """
+    points_index = load_points_index(points_index_file)
+    _, depths = load_depth_data_parser(DEPTH_DATA_PATH)
+    xsize, ysize, raster_count, geo_data_loader = gdal_reader(GEO_TIFF_FILE)
+    geo_data = geo_data_loader()
+
+    data = list()
+    margin = 1
+    
+    index = 0
+
+    for point in points_index:
+        if point:
+                index_y = point[0]
+                index_x = point[1]
+                point_pixel_data = geo_data[:, index_y:index_y+margin, index_x: index_x + margin]
+                point_depth_data = float(depths[index])
+                data.append([point_pixel_data, point_depth_data] )
+        else:
+            print("Cannot locate point:", index)
+
+        index += 1
+
+    obj_dump(data, file_name='func1_point.data')
+
+
 def mark_labels():
     """
     Function: to print labels in the hyperspectral image.
     """
-    X_test, ysize, raster_count, geo_data_loader = gdal_reader(GEO_TIFF_FILE)
+    xsize, ysize, raster_count, geo_data_loader = gdal_reader(GEO_TIFF_FILE)
     geo_data = geo_data_loader()
     band = numpy.copy(geo_data[90])
-    points_index_file = "points_index.info.20181229_063205"
+    # points_index_file = "points_index.info.20181229_063205"
+    points_index_file = "points_index.info.20181229_080434"
     points_index = load_points_index(points_index_file)
     _, depths = load_depth_data_parser(DEPTH_DATA_PATH)
-    print(len(depths))
-    margin = 3
+    margin = 4
     index = 0
+    duplicate = 0
+    unique_points_index = list()
     for point in points_index:
         if point:
-            index_y = point[0]
-            index_x = point[1]
-            depth = float(depths[index])
-            band[index_y -margin:index_y+margin, index_x -margin : index_x + margin] = depth * 5000
+            if point in unique_points_index:
+                pass
+                duplicate += 1
+            else:
+                unique_points_index.append(point)
+                index_y = point[0]
+                index_x = point[1]
+                depth = float(depths[index])
+                band[index_y -margin:index_y+margin, index_x -margin : index_x + margin] = depth * 5000
+        else:
+            print("Cannot locate point:", index)
         index += 1
+    
+    print('duplicate', duplicate)
     plt.figure('')
     plt.imshow(band)
     plt.show()
@@ -67,4 +109,6 @@ def test():
 
 if __name__ == '__main__':
     # test()
-    mark_labels()
+    # mark_labels()
+    points_index_file = "points_index.info.20181229_080434"
+    extract_pixel_value(points_index_file)
