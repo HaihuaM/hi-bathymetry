7d94c27b34b39e4567db15cd58d4fc87121031a7
haihuam <haihuam@outlook.com>
2019-01-15 15:10:46 +0000

    update

--- predict.py
+++ predict.py
@@ -16,6 +16,29 @@ from coordinate_transform import gdal_reader, load_depth_data_parser, obj_dump
 from sklearn import preprocessing
 import matplotlib.pyplot as plt
 
+
+def data_load_func1(func1_point_data):
+    """
+    Function: load_func1_data 
+    parameters: pixel data generate from func1.
+    returns:
+    """
+    data_file = op.join(OUTPUT_PATH, func1_point_data)
+    with open(data_file, 'rb') as data_file_handle:
+        data = pickle.load(data_file_handle)
+
+    features = list()
+    labels = list()
+    for item in data: 
+        features.append(item[0])
+        labels.append([item[1]])
+
+    x = np.array(features).squeeze()
+    y = np.array(labels)
+    train_x, test_x, train_y, test_y = train_test_split(x, y, train_size=0.8, random_state=33)
+    return train_x, test_x, train_y, test_y
+
+
 def model_predict(Model, checkpoint_dir):
 
 
@@ -23,19 +46,27 @@ def model_predict(Model, checkpoint_dir):
     out_file_name = '.'.join([func_name, 'data'])
 
     xsize, ysize, raster_count, geo_data_loader = gdal_reader(GEO_TIFF_FILE)
-    # print(xsize, ysize)
+    print(xsize, ysize)
     geo_data = geo_data_loader()
     transposed_data = np.reshape(geo_data, [-1, 330])
+    # print(transposed_data.shape)
 
-    ss_x = preprocessing.StandardScaler()
-    transposed_data = ss_x.fit_transform(transposed_data)
+    # func2_point_data = "extract_pixel_value_func2.data.20190107_043546"
+    # data_load_func2 = data_load_func1
+    # train_x, test_x, train_y, test_y = data_load_func2(func2_point_data)
 
-    print(transposed_data.shape)
-    sys.exit(0)
+    # ss_x = preprocessing.StandardScaler()
+    # transposed_data = ss_x.fit_transform(transposed_data)
 
+    # print(transposed_data.shape)
+    # sys.exit(0)
+
+    # print(test_x.shape)
+    # sys.exit(0)
     model = Model(test_x=transposed_data)
     model.setup_net()
-    prediction_value = model.predict(checkpoint_dir)
+    prediction_value = model.test_predict(checkpoint_dir)
+    print(prediction_value)
     print(prediction_value.shape)
     data = np.reshape(prediction_value, [ysize, xsize])
     print(data.shape)
@@ -43,5 +74,6 @@ def model_predict(Model, checkpoint_dir):
     
 
 if __name__ == "__main__":
-    model_predict(Model10, 'Model10_func2_20190115_103412')
+    # model_predict(Model10, 'Model10_func2_20190115_103412')
+    model_predict(Model1, 'Model1_func2_20190115_144146')
 
