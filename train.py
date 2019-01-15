36ac5fe403c9f64b52408303083493e548821ef0
haihuam <haihuam@outlook.com>
2019-01-15 10:48:25 +0000

    update

--- train.py
+++ train.py
@@ -13,6 +13,7 @@ import tensorflow as tf
 from global_envs import *
 from tqdm import tqdm
 from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
 from utility import time_stamp, LQueue
 # from models import Model1, Model2, Model3, Model4, Model5, Model6
 from models import *
@@ -44,6 +45,36 @@ def data_load_func1(func1_point_data):
     train_x, test_x, train_y, test_y = train_test_split(x, y, train_size=0.8, random_state=33)
     return train_x, test_x, train_y, test_y
 
+def data_load_func3(data_file):
+    """
+    Function: load_func1_data 
+    parameters: pixel data generate from func1.
+    returns:
+    """
+    # data_file = op.join(OUTPUT_PATH, func1_point_data)
+    with open(data_file, 'rb') as data_file_handle:
+        data = pickle.load(data_file_handle)
+
+    features = list()
+    labels = list()
+
+    for point in data:
+        features.append(data[point][0])
+        labels.append(data[point][1])
+
+    x = np.array(features)
+    y = np.array(labels)
+    train_x, test_x, train_y, test_y = train_test_split(x, y, train_size=0.8, random_state=33)
+
+    ss_x = preprocessing.StandardScaler()
+    train_x_ = ss_x.fit_transform(train_x)
+    test_x_ = ss_x.transform(test_x)
+
+    ss_y = preprocessing.StandardScaler()
+    train_y = train_y.reshape(-1, 1)
+    test_y = test_y.reshape(-1, 1)
+    
+    return train_x, test_x, train_y, test_y
 
 def model_retune(Model, checkpoint_dir):
     func2_point_data = "extract_pixel_value_func2.data.20190107_043546"
@@ -57,9 +88,10 @@ def model_retune(Model, checkpoint_dir):
     model.continue_on_train(checkpoint_dir, 100)
 
 def model_loader(Model):
-    func2_point_data = "extract_pixel_value_func2.data.20190107_043546"
+    # func2_point_data = "extract_pixel_value_func2.data.20190107_043546"
+    func2_point_data = "../legacy_data/averaged_points.info"
     data_load_func2 = data_load_func1
-    train_x, test_x, train_y, test_y = data_load_func2(func2_point_data)
+    train_x, test_x, train_y, test_y = data_load_func3(func2_point_data)
     model = Model(train_x, test_x, train_y, test_y, 0.001)
     # model.setup_net()
     model.setup_net()
@@ -70,6 +102,7 @@ def model_predict(Model, checkpoint_dir):
     data_load_func2 = data_load_func1
     train_x, test_x, train_y, test_y = data_load_func2(func2_point_data)
     # checkpoint_dir = "func2_checkpoint_dir_5000_0.001_20190107_044853"
+    # print(train_x.shape)
     model = Model(train_x, test_x, train_y, test_y, 0.001)
     # sys.exit(0)
     model.setup_net()
@@ -84,8 +117,8 @@ def train_func():
     # model_loader(Model4)
     # model_loader(Model5)
     # model_loader(Model6)
-    # model_loader(Model10)
-    model_predict(Model10, 'Model10_func2_20190115_065637')
+    model_loader(Model10)
+    # model_predict(Model10, 'Model10_func2_20190115_065637')
 
 
 if __name__ == "__main__":
