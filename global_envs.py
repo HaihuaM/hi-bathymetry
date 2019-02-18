a14ad53b25bd137953f18cd96584dbc6a6fd67bc
haihuam <haihuam@outlook.com>
2019-02-18 12:18:11 +0000

    update

--- global_envs.py
+++ global_envs.py
@@ -2,7 +2,7 @@
 import os.path as op
 
 DATA_PATH = '../orig_data/GF5_AHSI_E120.98_N31.81_20180715_000983_L10000006312/'
-DEPTH_DATA_NAME = "2018.7.15_corrected.txt"
+DEPTH_DATA_NAME = "depth_merged.txt"
 DEPTH_DATA_PATH = op.join('../orig_data', 'data.depth', DEPTH_DATA_NAME)
 
 LAT_TIFF_FILE = op.join(DATA_PATH, "GF5_AHSI_E120.98_N31.81_20180715_000983_L10000006312.lat.tiff")
