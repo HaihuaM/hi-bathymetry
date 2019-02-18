151cfacb818e807a7366a0b2342550de34b1e864
haihuam <haihuam@outlook.com>
2019-02-18 12:13:25 +0000

    update

--- coordinate_transform.py
+++ coordinate_transform.py
@@ -10,7 +10,7 @@ import pickle
 from osgeo import gdal
 from tqdm import tqdm
 from global_envs import *
-from utility import time_stamp
+from utility import time_stamp, debug_print
 import matplotlib.pyplot as plt
 
 global GEO_TIFF_FILE 
@@ -53,21 +53,12 @@ def locate_point(lat_value, lon_value, lat_data, lon_data, lat_xsize, lat_ysize)
     #     Lat: 31.56370
     """
     mapped_lat_data = numpy.where(lat_data >= lat_value, 1, 0)
-    # mapped_lon_data = numpy.where(lon_data >= lon_value, 1, 0)
-    # plt.figure('lat')
-    # plt.imshow(mapped_lat_data)
-    # plt.figure('lon')
-    # plt.imshow(mapped_lon_data)
-    # plt.figure('lat + lon')
-    # plt.imshow(mapped_lat_data + mapped_lon_data)
-    # plt.show()
-    # import sys
-    # sys.exit(0)
-    temp_y = numpy.sum(mapped_lat_data, axis=0)
-    temp_y = temp_y - 1
-    [temp_x] = numpy.where(temp_y < lat_ysize)
+    temp_y = numpy.sum(mapped_lat_data, axis=0) - 1 
+    # [temp_x] = numpy.where(temp_y < lat_ysize)
+    temp_x = numpy.array(range(len(temp_y)))
     candidates_list = list(zip(temp_y, temp_x))
     len_candidates = len(candidates_list)
+    y_max_len, x_max_len = lat_data.shape
 
     for i in range(len_candidates -1):
         index_y = candidates_list[i][0]
@@ -79,8 +70,17 @@ def locate_point(lat_value, lon_value, lat_data, lon_data, lat_xsize, lat_ysize)
         
         next_temp_lon_value = lon_data[next_index_y][next_index_x]
         if lon_value > temp_lon_value and lon_value < next_temp_lon_value:
-            return [index_y, index_x]
+            if (index_y + 1 < y_max_len ) and (index_x + 1 < x_max_len):
+                _temp_lat_value = lat_data[index_y, index_x]
+                _temp_next_lat_value = lat_data[index_y+1, index_x]
+                _temp_lon_value = lon_data[index_y, index_x]
+                _temp_next_lon_value = lon_data[index_y, index_x+1]
 
+                index_y += (lat_value - _temp_lat_value)/(_temp_next_lat_value - _temp_lat_value)
+                index_x += (lon_value - _temp_lon_value)/(_temp_next_lon_value - _temp_lon_value)
+                return [index_y, index_x]
+            else:
+                return []
     return []
 
 
@@ -105,7 +105,15 @@ def load_depth_data_parser(depth_file):
     return coordinates, depths
 
 
-def load_depth_data_coordinate(coordinates):
+def pixel_interpolation(coordinates):
+    """
+    Function: non-interger points, use bilinear interpolation
+    parameters: coordinates
+    return: pixel values
+    """
+
+
+def load_depth_data_coordinate(coordinates, depths, patch_size=0):
     """
     Function: read the coordinates data 
     parameters: list of the lat and lon data
@@ -120,25 +128,50 @@ def load_depth_data_coordinate(coordinates):
     lat_min = numpy.min(lat_data)
     lon_max = numpy.max(lon_data)
     lon_min = numpy.min(lon_data)
-    print("lat_max: %s, lat_min: %s, lon_max: %s, lon_min: %s"%(lat_max, lat_min, lon_max, lon_min))
+
+    x_patch_res = (lon_max - lon_min) / (lat_xsize - 1)
+    y_patch_res = (lat_max - lat_min) / (lat_ysize - 1)
+    assert(patch_size >=0 )
 
     index_list = list()
+    depth_list = list()
+    depth_index = 0
+    # for coord in tqdm(coordinates[32700:]):
     for coord in tqdm(coordinates):
+        depth = depths[depth_index]
         lat_value = float(coord[0])
         lon_value = float(coord[1])
         condition = ((lat_value <= lat_max and lat_value >= lat_min) and (lon_value <= lon_max and lon_value >= lon_min))
         if condition:
             index = locate_point(lat_value, lon_value, lat_data, lon_data, lat_xsize, lat_ysize)
-            index_list.append(index)
             if index:
-                pass
+                patch = build_patch(index, patch_size=patch_size)
+                index_list.append(patch)
+                depth_list.append(depth)
             else:
                 print("Waring: Cannot locate point:", coord)
         else:
             print("Info: Point <lat %s, lon %s> is out of scope" %(lat_value, lon_value))
+        depth_index += 1
 
-    return index_list
+    return index_list, depth_list
         
+def build_patch(index, patch_size=0):
+    """
+    Function: Build path index
+    parameters: one coord, patch size
+    return: tupple of coords list
+    """
+    index_y =  index[0]
+    index_x =  index[1]
+    patch_x_list = numpy.arange(-patch_size, patch_size+1, 1) + index_x
+    patch_y_list = numpy.arange(-patch_size, patch_size+1, 1) + index_y
+    return numpy.dstack(numpy.meshgrid(patch_y_list, patch_x_list))
+
+def test_build_patch():
+    index = [123.63463623, 326.11111]
+    x = build_patch(index, patch_size=1)
+    print(x)
 
 def obj_dump(dump_obj, file_name='out.dump'):
     """
@@ -164,11 +197,16 @@ def load_points_index(points_index_file):
     """
     with open(points_index_file, 'rb') as points_index_file_handle:
         points_index = pickle.load(points_index_file_handle)
+        return points_index
 
 
 def test():
-    lon_value = 121.3450
-    lat_value = 31.61876
+    # lon_value = 121.3450
+    # lat_value = 31.61876
+
+    lon_value = 121.353867
+    lat_value = 31.566635
+    # 121.353867   31.566635
     # 31.61876', '121.3450
 
     # ('31.60980', '121.3506')
@@ -186,30 +224,24 @@ def test():
     print(lat_value, lon_value)
     condition = ((lat_value <= lat_max and lat_value >= lat_min) and (lon_value <= lon_max and lon_value >= lon_min))
     if condition:
-
         INDEX = locate_point(lat_value, lon_value, lat_data, lon_data, lat_xsize, lat_ysize)
         print(INDEX)
     else:
         print("Info: Point <lat %s, lon %s> is out of scope" %(lat_value, lon_value))
-    # plt.figure('')
-    # plt.imshow(lat_data)
-    # plt.show()
-    # print(points_index)
-
 
 
 if __name__ == '__main__':
 
-    coordinates, _ = load_depth_data_parser(DEPTH_DATA_PATH)
-    index_list = load_depth_data_coordinate(coordinates)
-    file_name = 'points_index.info'
-    out_path = obj_dump(index_list, file_name)
-    # load_points_index(out_path)
+    coordinates, depths = load_depth_data_parser(DEPTH_DATA_PATH)
+    patch_size = 2
+    index_list, depth_list = load_depth_data_coordinate(coordinates, depths, patch_size)
+    # print(index_list)
     # print(len(index_list))
-    # test()
-    
-    
-
-
-
+    points_file_name = "points_index_patch_x%s.info" %patch_size
+    out_path = obj_dump(index_list, points_file_name)
+    # depth_file_name = 'filterd_depth.info'
+    # out_path = obj_dump(depth_list, depth_file_name)
+    # out_path = '../out/points_index_patch_x1.info.20190217_080559'
+    # points_index = load_points_index(out_path)
+    # debug_print(len(points_index))
 
